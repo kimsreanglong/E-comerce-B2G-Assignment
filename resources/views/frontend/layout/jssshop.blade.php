621dc15a2303ab57b05ce2a53707222cb9f3b8f7
unknown <longkimsreang09@gmail.com>
2025-08-03 21:00:35 +0700

    Check on styleshop and JSshop

--- resources/views/frontend/layout/jssshop.blade.php
+++ resources/views/frontend/layout/jssshop.blade.php
@@ -1,21 +1,21 @@
 
 	<!-- jQuery -->
-	<script src="{{asset('frontend/assets/js/jquery.min.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/jquery.min.js')}}"></script>
 	<!-- jQuery Easing -->
-	<script src="{{asset('frontend/assets/js/jquery.easing.1.3.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/jquery.easing.1.3.js')}}"></script>
 	<!-- Bootstrap -->
-	<script src="{{asset('frontend/assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/bootstrap.min.js')}}"></script>
 	<!-- Waypoints -->
-	<script src="{{asset('frontend/assets/js/jquery.waypoints.min.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/jquery.waypoints.min.js')}}"></script>
 	<!-- Carousel -->
-	<script src="{{asset('frontend/assets/js/owl.carousel.min.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/owl.carousel.min.js')}}"></script>
 	<!-- countTo -->
-	<script src="{{asset('frontend/assets/js/jquery.countTo.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/jquery.countTo.js')}}"></script>
 	<!-- Magnific Popup -->
-	<script src="{{asset('frontend/assets/js/jquery.magnific-popup.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/magnific-popup-options.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/magnific-popup-options.js')}}"></script>
 	<!-- Main -->
-	<script src="{{asset('frontend/assets/js/main.js')}}"></script>
+	<script src="{{asset('Frontend/Assets/js/main.js')}}"></script>
 
 
 
